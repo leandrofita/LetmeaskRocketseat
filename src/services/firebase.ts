c5ec3057b1390f3f5a14a968017650a6320283aa
Leandro Fita <leandrocfita@gmail.com>
2022-03-18 00:26:09 -0300

    teste de deploy e remoção das variáveis de ambiente

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -10,15 +10,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
-
-/*   apiKey: "AIzaSyCQadISONL2SfSUFAS_1cXPZRMcAwiiLLk",
-  authDomain: "letmeask-c4c4a.firebaseapp.com",
-  databaseURL: "https://letmeask-c4c4a-default-rtdb.firebaseio.com",
-  projectId: "letmeask-c4c4a",
-  storageBucket: "letmeask-c4c4a.appspot.com",
-  messagingSenderId: "636181704961",
-  appId: "1:636181704961:web:d2fb788b734e0afe18eb00", */
-
 };
 
 const app = initializeApp(firebaseConfig);
